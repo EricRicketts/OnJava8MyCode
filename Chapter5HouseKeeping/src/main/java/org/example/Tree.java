2e41413160d9d3faac3607c253ffbac969dba85a
Eric Ricketts <eric_ricketts@me.com>
2024-07-14 07:37:47 -0400

    Overloaded constructors and methods

--- Chapter5HouseKeeping/src/main/java/org/example/Tree.java
+++ Chapter5HouseKeeping/src/main/java/org/example/Tree.java
@@ -11,6 +11,11 @@ public class Tree {
         this.height = height;
     }
 
+    // below we have two versions of method "info" this is an example of method overloading
+    // as with constructors each of the overloaded methods must take a different argument list
+    // note the methods can actually have the same parameters but in different order this would
+    // still qualify as an overloaded method but it is bad practice making the code more difficult
+    // to maintain
     public String info() {
         return "Tree has height = " + height + " feet";
     }
@@ -19,6 +24,7 @@ public class Tree {
         return s + ": Tree has height = " + height + " feet";
     }
 
+    // below we have two constructors, this is an example of constructor overloading
     public Tree(int height) {
         this.height = height;
     }
