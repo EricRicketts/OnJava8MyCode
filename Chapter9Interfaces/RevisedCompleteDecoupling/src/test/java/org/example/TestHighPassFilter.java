a855d6c609f331d6184a9da5636fdf898807a8f9
Eric Ricketts <eric_ricketts@me.com>
2024-08-17 14:17:00 -0400

    Test HighPass filter

---
+++ Chapter9Interfaces/RevisedCompleteDecoupling/src/test/java/org/example/TestHighPassFilter.java
@@ -0,0 +1,50 @@
+package org.example;
+
+import org.apache.commons.lang3.ArrayUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestHighPassFilter {
+
+    private double expected, actual;
+    private Waveform waveform;
+    private HighPass highPass;
+
+    @BeforeEach
+    public void setUp() {
+        waveform = new Waveform();
+        highPass = new HighPass(5.25);
+    }
+
+    @Test
+    @Order(1)
+    public void testHighPassApplicatorApply() {
+        String[] expectedStrings = new String[]{
+            "Using Processor HighPass Waveform [id = 0]" + "\n",
+            "Using Processor HighPass Waveform [id = 1]" + "\n"
+        };
+        String actualString = Applicator.apply(new FilterAdapter(new HighPass(5.25)), waveform);
+        assertTrue(ArrayUtils.contains(expectedStrings, actualString));
+    }
+
+    @Test
+    @Order(2)
+    public void testGetHighPassCutOff() {
+        expected = 5.25;
+        actual = highPass.getCutoff();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @Order(3)
+    public void testSetHighPassFilter() {
+        expected = 3.33;
+        highPass.setCutoff(3.33);
+        actual = highPass.getCutoff();
+        assertEquals(expected, actual);
+    }
+}
